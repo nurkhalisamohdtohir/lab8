ec559f0cc028e2d09acb4e7317f4116cae56ae57
Nurkhalisa Mohd Tohir <lisatohir@gmail.com>
2021-03-18 18:48:40 +0800

    First commit for Location.java

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,16 @@
+package samples.demo;
+
+public class Location {
+	
+	private String name;
+	private int locationId;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
